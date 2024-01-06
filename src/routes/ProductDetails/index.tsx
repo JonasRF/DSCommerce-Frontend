e06749b5375e0c409e5d6fbf5608bf239c35e4a1
Jonas Ribeiro Ferreira <jonas.ribeiro@fiocruz.br>
2024-01-06 17:58:02 -0300

    Componentes da página de Catálogo

---
+++ src/routes/ProductDetails/index.tsx
@@ -0,0 +1,21 @@
+import ButtonInverse from "../../components/ButtonInverse";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import HeaderClient from "../../components/HeaderClient";
+import ProductDetailsCard from "../../components/ProductDetailsCard";
+
+export default function ProductDetails() {
+    return(
+    <>
+    <HeaderClient />
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <ProductDetailsCard />
+          <div className="dsc-btn-container">
+           <ButtonPrimary />
+           <ButtonInverse />
+          </div>
+        </section>
+      </main>
+    </>
+    );
+}
