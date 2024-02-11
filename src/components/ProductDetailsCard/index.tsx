3fa00c72447b2228f2f2154f3dc0e98af54a9460
JonasRF <jonasribeiro41@yahoo.com.br>
2024-02-11 19:37:01 -0300

    Aplicando props e rotas nos componentes

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -1,30 +1,27 @@
-import  computerImg  from '../../assets/computer.svg';
+
+import { ProductDTO } from '../../models/product';
 import ProductCategory from '../ProductCategory';
 import './styles.css';
 
-export default function ProductDetailsCard() {
+type Props = {
+  product: ProductDTO;
+}
+
+export default function ProductDetailsCard({ product }: Props) {
   return (
     <div className="dsc-card dsc-mb20">
       <div className="dsc-product-details-top dsc-line-bottom">
-        <img src={computerImg} alt="computador" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="dsc-product-details-bottom">
-        <h3>R$ 5000,00</h3>
-        <h4>Computador Gamer XT</h4>
+        <h3>{product.price.toFixed(2)}</h3>
+        <h4>{product.name}</h4>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard d ummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          {product.description}
         </p>
         <div className="dsc-category-container">
-          <ProductCategory />
-          <ProductCategory />
+          <ProductCategory name="Eletrônicos" />
+          <ProductCategory name="Computadores" />
         </div>
       </div>
     </div>
