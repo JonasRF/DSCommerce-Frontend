d52ba4aa5566e9dc570aecaf03355dff1a9d9c3a
JonasRF <jonasribeiro41@yahoo.com.br>
2024-02-26 13:22:44 -0300

    Save object OrderDTO locslStorage

---
+++ src/services/cart-service.ts
@@ -0,0 +1,6 @@
+import { OrderDTO } from "../models/order";
+import * as cartRepository from '../localStorage/cart-repository';
+
+export function saveCart(cart: OrderDTO) {
+    cartRepository.save(cart);
+}
