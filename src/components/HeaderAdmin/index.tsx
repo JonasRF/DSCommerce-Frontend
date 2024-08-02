05c6a4b89543cb30147f32dad0bcc3e95dce4450
JonasRF <jonasribeiro41@yahoo.com.br>
2024-08-01 23:49:23 -0300

    Administrative area navigation bar

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import homeIcon from '../../assets/home.svg';
 import productIcon from '../../assets/products.svg';
 import LoggedUser from "../LoggedUser";
+import { NavLink } from "react-router-dom";
 
 export default function HeaderAdmin() {
 
@@ -11,19 +12,22 @@ export default function HeaderAdmin() {
         <h1>Admin</h1>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-itens-container">
+          <NavLink  className="dsc-menu-inicio" to="/admin/home">
             <div className="dsc-menu-item">
               <img src={homeIcon} alt="Inicio" />
             </div >
             <div className="dsc-menu-inicio">
               <p>Inicio</p>
             </div>
-
+            </NavLink>
+            <NavLink  className="dsc-menu-inicio" to="/admin/products">
             <div className="dsc-menu-item">
               <img src={productIcon} alt="Cadastro de produtos" /> 
             </div>
             <div className="dsc-menu-inicio">
               <p>Produtos</p>
             </div>
+            </NavLink> 
             <LoggedUser />
           </div>
         </div>
