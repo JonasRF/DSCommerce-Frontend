6c0fa9c32385e6867b0077d87197657db0a70c36
JonasRF <jonasribeiro41@yahoo.com.br>
2024-12-18 16:36:16 -0300

    handleEditClick

--- src/routes/ClientHome/Admin/ProductListing/index.tsx
+++ src/routes/ClientHome/Admin/ProductListing/index.tsx
@@ -62,6 +62,10 @@ export default function ProductListing() {
     setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
+  const handleEditClick = (productId: number) => {
+    navigate(`/admin/products/${productId}`);
+  }
+
   const handleDeleteClick = (productId: number) => {
     setDialogConfirmationData({ ...dialogConfirmationData, id: productId, visible: true });
   }
@@ -123,7 +127,7 @@ export default function ProductListing() {
                   <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name} /></td>
                   <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
                   <td className="dsc-txt-left">{product.name}</td>
-                  <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Edit" /></td>
+                  <td><img onClick={() => handleEditClick(product.id)} className="dsc-product-listing-btn" src={deleteIcon} alt="Edit" /></td>
                   <td><img onClick={() => handleDeleteClick(product.id)} className="dsc-product-listing-btn" src={editIcon} alt="Delete" /></td>
                 </tr>
               ))
